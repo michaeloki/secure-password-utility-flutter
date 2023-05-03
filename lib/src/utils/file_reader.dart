429e49c7a16ab408827d75561229bc264bcc3b96
Michael Oki <mikeoki.oki@gmail.com>
2023-05-03 18:51:42 +0200

    Removed redundant code

--- lib/src/utils/file_reader.dart
+++ lib/src/utils/file_reader.dart
@@ -4,8 +4,6 @@ import '../constants.dart';
 var passwordStatus = false;
 var receivedPassword = '';
 Future<bool> findWeakPasswords(String rawPassword) async {
-  //receivedPassword = rawPassword;
-  //await readJsonFile(filePath, receivedPassword);
   var input = await File(filePath).readAsLines();
   input.forEach((value) => {
         if (rawPassword
@@ -17,17 +15,3 @@ Future<bool> findWeakPasswords(String rawPassword) async {
       });
   return passwordStatus;
 }
-
-// Future<bool> readJsonFile(String filePath, String password) async {
-//   var input = await File(filePath).readAsLines();
-//   input.forEach((value) => {
-//         if (password
-//                 .toLowerCase()
-//                 .trim()
-//                 .contains(value.toLowerCase().trim()) &&
-//             value.length >= 3)
-//           {passwordStatus = true}
-//       });
-//
-//   return passwordStatus;
-// }
