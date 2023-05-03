3e22f64da761c2d21c090164e188c66034131e0d
Michael Oki <mikeoki.oki@gmail.com>
2023-05-03 18:44:29 +0200

    Optimised filereader

--- lib/src/utils/file_reader.dart
+++ lib/src/utils/file_reader.dart
@@ -4,21 +4,30 @@ import '../constants.dart';
 var passwordStatus = false;
 var receivedPassword = '';
 Future<bool> findWeakPasswords(String rawPassword) async {
-  receivedPassword = rawPassword;
-  await readJsonFile(filePath, receivedPassword);
-  return passwordStatus;
-}
-
-Future<bool> readJsonFile(String filePath, String password) async {
+  //receivedPassword = rawPassword;
+  //await readJsonFile(filePath, receivedPassword);
   var input = await File(filePath).readAsLines();
   input.forEach((value) => {
-        if (password
-                .toLowerCase()
-                .trim()
-                .contains(value.toLowerCase().trim()) &&
-            value.length >= 3)
-          {passwordStatus = true}
-      });
-
+    if (rawPassword
+        .toLowerCase()
+        .trim()
+        .contains(value.toLowerCase().trim()) &&
+        value.length >= 3)
+      {passwordStatus = true}
+  });
   return passwordStatus;
 }
+
+// Future<bool> readJsonFile(String filePath, String password) async {
+//   var input = await File(filePath).readAsLines();
+//   input.forEach((value) => {
+//         if (password
+//                 .toLowerCase()
+//                 .trim()
+//                 .contains(value.toLowerCase().trim()) &&
+//             value.length >= 3)
+//           {passwordStatus = true}
+//       });
+//
+//   return passwordStatus;
+// }
